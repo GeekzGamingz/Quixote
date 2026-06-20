fbb7f5c31be057967360c9c92d263ef70d782948
Zeuk <GeekzGamingz@gmail.com>
2026-06-20 07:09:38 -0400

    Replace Arbitrary Values

--- source/03 - Entities/00 - Quixote/Quixote.gd
+++ source/03 - Entities/00 - Quixote/Quixote.gd
@@ -3,7 +3,13 @@ extends CharacterBody2D
 signal quixote_damage
 #------------------------------------------------------------------------------#
 #Variables
+var geriatric_damage: int = 10
 #Exported Variables
+@export_category("Geriatric Damage")
+@export_range(10, 30, 5, "prefer_slider") var geriatric_damage_base = geriatric_damage
+@export_range(10, 30, 5, "prefer_slider") var geriatric_damage_max = geriatric_damage
+@export_range(5, 10, 1, "prefer_slider") var geriatric_ticks: = 10
+@export_category("Traits")
 @export_range(1, 10, 1, "prefer_slider") var horse_speed: int
 #OnReady Variables
 #Main Nodes
@@ -20,14 +26,20 @@ func _ready() -> void:
 	MAIN.WINDMILL.connect("spin", impede)
 #------------------------------------------------------------------------------#
 #Signaled Functions
-func _on_geriatric_timeout() -> void: emit_signal("quixote_damage", "Geriatric", 20)
+func _on_geriatric_timeout() -> void: emit_signal("quixote_damage", "Geriatric", geriatric_damage)
 #------------------------------------------------------------------------------#
 #Custom Functions
 func ride_forth(delta): global_position.x += horse_speed * delta
 #------------------------------------------------------------------------------#
 #Custom Signaled Functions
 func impede(spinning, wind_speed):
-	if spinning: horse_speed -= wind_speed
-	else: horse_speed = starting_speed
+	if spinning:
+		horse_speed -= wind_speed
+		geriatric_timer.wait_time = 5.0
+		geriatric_damage = geriatric_damage_max
+	else:
+		horse_speed = starting_speed
+		geriatric_timer.wait_time = 10.0
+		geriatric_damage = geriatric_damage_base
 	print("Wind Speed: ", wind_speed)
 	print("Horse Speed: ", horse_speed)
